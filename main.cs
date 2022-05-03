53984c508504baa6d888018e4f2c32a906bd5696
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-03 16:51:07 +0000

    changed spacing

--- main.cs
+++ main.cs
@@ -9,20 +9,20 @@ class Program
 {
     public static void Main(string[] args)
     {
-      int num;
-      Console.Write("Determine what movie a person is aloud to watch based off of their age:");
-      Console.ReadLine();
-      Console.Write("Enter your age : ");
-      num = Convert.ToInt32(Console.ReadLine());
-      if (num == 17) {
-        Console.WriteLine("You can see an R rated movie alone!");
-      } else if (num == 13) {
-        Console.WriteLine("You can see a PG 13 rated movie alone!"));
-      } else if (num == 5) {
-        Console.WriteLine("You can see a G rated movie alone!");
-      } else {
-        Console.WriteLine("Uh. You are too young for most things!");
+        int num;
+        Console.Write("Determine what movie a person is aloud to watch based off of their age:");
+        Console.ReadLine();
+        Console.Write("Enter your age : ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num == 17) {
+            Console.WriteLine("You can see an R rated movie alone!");
+        } else if (num == 13) {
+            Console.WriteLine("You can see a PG 13 rated movie alone!"));
+        } else if (num == 5) {
+            Console.WriteLine("You can see a G rated movie alone!");
+        } else {
+            Console.WriteLine("Uh. You are too young for most things!");
         }
-      Console.WriteLine("\nDone.");
+          Console.WriteLine("\nDone.");
     }
 }
