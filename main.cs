77354433310e76216653bac04211d51ab2ef2cb3
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-04 23:33:44 +0000

    added space

--- main.cs
+++ main.cs
@@ -14,11 +14,11 @@ class Program
         Console.ReadLine();
         Console.Write("Enter your age : ");
         num = Convert.ToInt32(Console.ReadLine());
-        if(num == 17) {
+        if (num == 17)  {
             Console.WriteLine("You can see an R rated movie alone!");
-        }   else if(num == 13) {
+        }   else if (num == 13) {
             Console.WriteLine("You can see a PG 13 rated movie alone!");
-        }   else if(num == 5) {
+        }   else if (num == 5) {
             Console.WriteLine("You can see a G rated movie alone!");
         }   else {
             Console.WriteLine("Uh. You are too young for most things!");
