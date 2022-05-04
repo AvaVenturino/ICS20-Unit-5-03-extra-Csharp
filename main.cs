1dc0fdd0743d131d3b73e45abdf8a0c588ad93ba
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-04 22:35:11 +0000

    changed cs

--- main.cs
+++ main.cs
@@ -16,9 +16,9 @@ class Program
         num = Convert.ToInt32(Console.ReadLine());
       if (num == 17) {
             Console.WriteLine("You can see an R rated movie alone!");
-      } else if ( num == 13 ) {
+      } else if (num == 13) {
             Console.WriteLine("You can see a PG 13 rated movie alone!");
-      } else if ( num == 5 ) {
+      } else if (num == 5) {
             Console.WriteLine("You can see a G rated movie alone!");
       } else {
             Console.WriteLine("Uh. You are too young for most things!");
